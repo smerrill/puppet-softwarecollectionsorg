c005f6eef9c2c83e52aacec070920bfc6913058e
Steven Merrill <steven.merrill@gmail.com>
2014-04-29 18:56:42 -0400

    Fix up error redefining $scl_url.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@
 # [*title*]
 #   The name of the Software Collection to install.
 # [*scl_url*]
-#   The URL to the release package for the Software Collection. This should be
+#   The URL to the repository for the Software Collection. This can be
 #   automatically calculated based on the name of the Software Collection.
 #
 define softwarecollectionsorg (
@@ -23,12 +23,15 @@ define softwarecollectionsorg (
   }
 
   if $scl_url == undef {
-    $scl_url = "https://www.softwarecollections.org/repos/rhscl/${title}/epel-6-x86_64"
+    $baseurl = "https://www.softwarecollections.org/repos/rhscl/${title}/epel-6-x86_64"
+  }
+  else {
+    $baseurl = $scl_url
   }
 
   yumrepo { "rhscl-${title}-epel-6-x86_64":
     descr    => "${title} epel-6-x86_64",
-    baseurl  => $scl_url,
+    baseurl  => $baseurl,
     enabled  => 1,
     gpgcheck => 0,
   }
